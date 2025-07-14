2d94b3353c5f449624c4f124287ed050d8a10cd3
AJ Caldwell <tomatrow@gmail.com>
2025-07-14 15:23:53 -0700

    Use mcp aider branch

--- scripts/aider.fish
+++ scripts/aider.fish
@@ -1,8 +1,9 @@
 #!/usr/bin/env fish
 
+# we're using the mcp pr branch right now. Change it back to `--with aider-chat \` when merged
 uv run \
   --python python3.12 \
-  --with aider-chat \
+  --with git+https://github.com/quinlanjager/aider@feature/litellm-mcp \
   --with google-cloud-aiplatform \
   '/Users/ajcaldwell/Library/Application Support/Nova/Extensions/dev.ajcaldwell.aider/nova-aider.py' \
   --config .aider.conf.yaml
