247944550b8903398bca339d7aefc34e711b5f3c
AJ Caldwell <tomatrow@gmail.com>
2025-07-14 14:51:36 -0700

    Reorganize the examples

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -1,24 +1,6 @@
-<script lang="ts">
-	let places = $state<Record<string, any>[]>([]);
-
-	async function load() {
-		const response = await fetch('http://localhost:3000/api/v1/places');
-		places = await response.json();
-	}
-
-	$effect(() => {
-		load();
-	});
-
-	$inspect(places);
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-{#each places as place (place.id)}
-	<div>
-		<h5>{place.name}</h5>
-		<p>{place.description}</p>
-	</div>
-{/each}
+<ul>
+	<li><a href="/places">places</a></li>
+	<li><a href="/todos">todos</a></li>
+	<li><a href="/paraglide-demo">paraglide demo</a></li>
+	
+</ul>
