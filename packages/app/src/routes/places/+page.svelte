ec9117988d599659f0717f565b57c43fc1548a22
AJ Caldwell <tomatrow@gmail.com>
2025-07-15 16:21:31 -0700

    Use a table

--- packages/app/src/routes/places/+page.svelte
+++ packages/app/src/routes/places/+page.svelte
@@ -5,20 +5,47 @@
 	let dialog: HTMLDialogElement
 </script>
 
-<h2>Places</h2>
+<h1>Places</h1>
 
-{#each await places as place (place.id)}
-	<div>
-		<h3>{place.name}</h3>
-		<p>{place.description}</p>
-		<p>{place.address}</p>
-		<button onclick={() => deletePlace(place.id)}>Delete</button>
-	</div>
-{/each}
-
-<button onclick={() => dialog.show()}>Add Place</button>
+<table>
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Address</th>
+			<th>Description</th>
+			<th>Phone</th>
+			<th>Website</th>
+			<th>Hours</th>
+			<th>Coordinates</th>
+			<th><button onclick={() => dialog.show()}>Add Place</button></th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each await places as place (place.id)}
+			<tr>
+				<td>{place.name}</td>
+				<td>{place.address}</td>
+				<td>{place.description}</td>
+				<td>{place.phone}</td>
+				<td>
+					{#if place.website}
+						<a href={place.website} target="_blank">site</a>
+					{/if}
+				</td>
+				<td>{place.hours}</td>
+				<td>
+					{#if place.latitude && place.longitude}
+						{place.latitude}, {place.longitude}
+					{/if}
+				</td>
+				<td><button onclick={() => deletePlace(place.id)}>Delete</button> </td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
 
 <dialog bind:this={dialog}>
+	<h2>New Place</h2>
 	<form {...addPlaceForm}>
 		<input name="name" placeholder="Place name" required />
 		<input name="address" placeholder="Address" required />
