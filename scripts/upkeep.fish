a5a048e17c62bdb5e6827235c790f6c53e72e949
AJ Caldwell <tomatrow@gmail.com>
2025-07-29 15:09:41 -0700

    Rework llms a bit

--- scripts/upkeep.fish
+++ scripts/upkeep.fish
@@ -7,7 +7,9 @@ curl -L https://unpkg.com/simpledotcss/simple.css | string replace --all 'body '
 pnpx prettier --write packages/app/static/simple.css
 
 # grab latest svelte llm context 
-curl https://svelte-llm.khromov.se/svelte-distilled > scripts/aider/svelte-distilled.txt
+curl https://svelte.dev/llms-small.txt > scripts/llms.txt/svelte.dev/llms-small.txt 
+curl https://svelte.dev/docs/kit/llms-small.txt > scripts/llms.txt/svelte.dev/docs/kit/llms-small.txt
+curl https://svelte.dev/docs/svelte/llms-small.txt > scripts/llms.txt/svelte.dev/docs/svelte/llms-small.txt
 
 # check outdated
 pnpm outdated
